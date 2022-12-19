bd3c72f85a589dde705c7808d6f797a438f8efdb
Scott Yuan <scoworthyuan@gmail.com>
2022-12-19 20:10:33 +1100

    installed service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -50,6 +51,12 @@ module.exports = {
 
   ),new MiniCssExtractPlugin(),
   new CssMinimizerPlugin(),
+  new WorkboxPlugin.GenerateSW({
+    // these options encourage the ServiceWorkers to get in there fast
+    // and not allow any straggling "old" SWs to hang around
+    clientsClaim: true,
+    skipWaiting: true,
+  }),
 ],
   optimization: {
     minimize: true,
